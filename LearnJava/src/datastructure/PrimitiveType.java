72bdc2f82528fa1c1cfe408f519e4d10e5794316
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-02 17:31:29 +0530

    formatting

--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -3,13 +3,13 @@ package datastructure;
 public class PrimitiveType {
     public static void main(String[] args) {
         String s = "the sum of the numbers are";
-        int num1=1;
-        int num2=2;
-        int num3=3;
-        char num4='s';
-        float num5=9.4f;
-
-        int sum=num1+num2+num3;
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        char num4 = 's';
+        float num5 = 9.4f;
+        int sum = num1 + num2 + num3;
         System.out.println(s);
-        System.out.println(sum);    }
+        System.out.println(sum);
+    }
 }
