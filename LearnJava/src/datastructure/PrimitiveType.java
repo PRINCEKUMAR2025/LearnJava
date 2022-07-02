59a7eb98317d0d02ab46e2d6d3ed69bd21d16860
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-02 17:27:40 +0530

    add 3 numbers

--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -2,7 +2,14 @@ package datastructure;
 
 public class PrimitiveType {
     public static void main(String[] args) {
-        System.out.println("Hello java");
+        String s = "the sum of the numbers are";
+        int num1=1;
+        int num2=2;
+        int num3=3;
+        char num4='s';
+        float num5=9.4f;
 
-    }
+        int sum=num1+num2+num3;
+        System.out.println(s);
+        System.out.println(sum);    }
 }
--- LearnJava/src/datastructure/practice.java
+++
@@ -1,4 +0,0 @@
-package datastructure;
-
-public class practice {
-}
