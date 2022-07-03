1c3ebfacf3bd69c671c8c4cd61329c14312d91ec
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-03 22:43:40 +0530

    CBSE Board Percentage Calculator

--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class PrimitiveType {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter no 1");
-        float num1 = sc.nextFloat();
-        System.out.println("enter no 2");
-        float num2 = sc.nextFloat();
-        float sum = num1 + num2;
+        System.out.println("enter highest marks for each subject");
+        float max=sc.nextFloat();
+        float maximum=max*5;
+        System.out.println("enter marks in 1st subject out of 100");
+        float a = sc.nextFloat();
+        System.out.println("enter marks in 2nd subject out of 100");
+        float b = sc.nextFloat();
+        System.out.println("enter marks in 3rd subject out of 100");
+        float c = sc.nextFloat();
+        System.out.println("enter marks in 4th subject out of 100");
+        float d = sc.nextFloat();
+        System.out.println("enter marks in 5th subject out of 100");
+        float e = sc.nextFloat();
+        float sum = a + b + c + d + e;
         System.out.println(sum);
+        float percentage = (sum / maximum)*100;
+        System.out.println(percentage);
     }
 }
