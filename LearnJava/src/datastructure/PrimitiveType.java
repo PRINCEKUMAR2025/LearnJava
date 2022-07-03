dc6ee3f4836fabd474413b0c47a42b8142c678c2
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-03 23:30:00 +0530

    3 subjects cgpa

--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class PrimitiveType {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter km i will change it into metre");
-        float km=sc.nextFloat();
-        float meter=km*1000;
-        System.out.println(meter);
+        float subject=3;
+        float s1=sc.nextFloat();
+        float s2= sc.nextFloat();
+        float s3= sc.nextFloat();
+        float sum=s1+s2+s3;
+        float cgpa=sum/subject;
+        System.out.println(cgpa);
+
     }
 }
