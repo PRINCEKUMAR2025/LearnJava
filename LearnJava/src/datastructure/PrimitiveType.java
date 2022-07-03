e1ee8de20d28d58c4d0273963ecc1604358ebb8b
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-03 21:10:43 +0530

    taking input from user

--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -1,15 +1,15 @@
 package datastructure;
 
+import java.util.Scanner;
+
 public class PrimitiveType {
     public static void main(String[] args) {
-        String s = "the sum of the numbers are";
-        int num1 = 1;
-        int num2 = 2;
-        int num3 = 3;
-        char num4 = 's';
-        float num5 = 9.4f;
-        int sum = num1 + num2 + num3;
-        System.out.println(s);
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = Sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = Sc.nextInt();
+        int sum = a + b;
         System.out.println(sum);
     }
 }
