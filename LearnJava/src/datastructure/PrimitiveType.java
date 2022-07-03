961154b46f687518692880e847648fd5c5aa5eb5
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-03 23:38:37 +0530

    greeting message

--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -5,13 +5,8 @@ import java.util.Scanner;
 public class PrimitiveType {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        float subject=3;
-        float s1=sc.nextFloat();
-        float s2= sc.nextFloat();
-        float s3= sc.nextFloat();
-        float sum=s1+s2+s3;
-        float cgpa=sum/subject;
-        System.out.println(cgpa);
+        String a=sc.nextLine();
+        System.out.println("Hello "+ a +" have a good day");
 
     }
 }
