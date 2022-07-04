0fcce5f5952975c4a314593fad5a61b0b2dddc37
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-04 14:02:48 +0530

    CBSE board % calculator

--- LearnJava/src/datastructure/Operators.java
+++ LearnJava/src/datastructure/Operators.java
@@ -1,13 +1,26 @@
 package datastructure;
 
+import java.util.Scanner;
+
 public class Operators {
     public static void main(String[] args) {
-       float a=20;
-       float b=100;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a/b);
-        System.out.println(a*b);
-        System.out.println(a%b);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter highest marks for each subject");
+        float max=sc.nextFloat();
+        float maximum=max*5;
+        System.out.println("enter marks in 1st subject out of 100");
+        float a = sc.nextFloat();
+        System.out.println("enter marks in 2nd subject out of 100");
+        float b = sc.nextFloat();
+        System.out.println("enter marks in 3rd subject out of 100");
+        float c = sc.nextFloat();
+        System.out.println("enter marks in 4th subject out of 100");
+        float d = sc.nextFloat();
+        System.out.println("enter marks in 5th subject out of 100");
+        float e = sc.nextFloat();
+        float sum = a + b + c + d + e;
+        System.out.println(sum);
+        float percentage = (sum / maximum)*100;
+        System.out.println(percentage);
     }
 }
