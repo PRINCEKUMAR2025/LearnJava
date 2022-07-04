42e3cd59060bd97d3de953b882986386d1b46a7b
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-04 13:40:13 +0530

    just creating new file in datastructure

---
+++ LearnJava/src/datastructure/Operators.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+public class Operators {
+    public static void main(String[] args) {
+       float a=20;
+       float b=100;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+    }
+}
