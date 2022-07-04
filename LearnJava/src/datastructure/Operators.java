71cbf8ea2977f3b7c0a648933cd5e26e414a3b3d
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-04 19:44:58 +0530

    area of rectangle

--- LearnJava/src/datastructure/Operators.java
+++ LearnJava/src/datastructure/Operators.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class Operators {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter length of rectangle");
-        float length=sc.nextFloat();
-        System.out.println("enter the breadth");
-        float breadth=sc.nextFloat();
-        float area=length*breadth;
-        System.out.println("the area is "+area);
+        System.out.println("entre first number");
+        float a=sc.nextFloat();
+        System.out.println("enter second number");
+        float b=sc.nextFloat();
+        if (a==b){
+            System.out.println("both are equal");
+        }
+        else{
+            System.out.println("both are unequal");
+        }
     }
 }
