a9cc97135b89977b75c59647ae71e089cf425cd5
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-04 19:37:44 +0530

    q

--- LearnJava/src/datastructure/Operators.java
+++ LearnJava/src/datastructure/Operators.java
@@ -5,22 +5,11 @@ import java.util.Scanner;
 public class Operators {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter highest marks for each subject");
-        float max=sc.nextFloat();
-        float maximum=max*5;
-        System.out.println("enter marks in 1st subject out of 100");
-        float a = sc.nextFloat();
-        System.out.println("enter marks in 2nd subject out of 100");
-        float b = sc.nextFloat();
-        System.out.println("enter marks in 3rd subject out of 100");
-        float c = sc.nextFloat();
-        System.out.println("enter marks in 4th subject out of 100");
-        float d = sc.nextFloat();
-        System.out.println("enter marks in 5th subject out of 100");
-        float e = sc.nextFloat();
-        float sum = a + b + c + d + e;
-        System.out.println(sum);
-        float percentage = (sum / maximum)*100;
-        System.out.println(percentage);
+        System.out.println("enter length of rectangle");
+        float length=sc.nextFloat();
+        System.out.println("enter the breadth");
+        float breadth=sc.nextFloat();
+        float area=length*breadth;
+        System.out.println("the area is "+area);
     }
 }
--- LearnJava/src/datastructure/PrimitiveType.java
+++ LearnJava/src/datastructure/PrimitiveType.java
@@ -2,11 +2,14 @@ package datastructure;
 
 import java.util.Scanner;
 
-public class PrimitiveType {
+public class Operators {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String a=sc.nextLine();
-        System.out.println("Hello "+ a +" have a good day");
-
+        System.out.println("enter length of rectangle");
+        float length=sc.nextFloat();
+        System.out.println("enter the breadth");
+        float breadth=sc.nextFloat();
+        float area=length*breadth;
+        System.out.println("the area is "+area);
     }
 }
