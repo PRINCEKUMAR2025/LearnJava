3adf8f21b49897dc33cf018f1652f2f4b71e7a22
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-09 00:54:37 +0530

    strings  methodss

--- LearnJava/src/datastructure/StringExample.java
+++ LearnJava/src/datastructure/StringExample.java
@@ -1,10 +1,33 @@
 package datastructure;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class StringExample {
     public static void main(String[] args) {
+
         String s1 = "Prince";
-        String s2 = "Prince";
-        System.out.println(s1==s2);
+//        String st1=new String("prince");
+//        String s2 = "Prince";
+//        System.out.print("the name is ");
+//        System.out.println(s1);
+//        int a=1;
+//        float b=2f;
+//        System.out.printf("%d\n%f",a,b);
+//        Scanner sc=new Scanner(System.in);
+//        String s3=sc.nextLine();
+//        System.out.println(s3);
+        String name="Prince";
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase(Locale.ROOT));
+        System.out.println(name.toUpperCase(Locale.ROOT));
+        int length=name.length();
+//        System.out.println(length);
+//        String lower=name.toLowerCase();
+//        System.out.println(lower);
+//        String upper=name.toUpperCase();
+//        System.out.println(upper);
+
+
 
     }
 }
