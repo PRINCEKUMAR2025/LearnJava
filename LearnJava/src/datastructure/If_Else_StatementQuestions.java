54ae1e76581fad7b70a5ecdf805f0f88779fb8e2
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-13 19:39:50 +0530

    which website

--- LearnJava/src/datastructure/If_Else_StatementQuestions.java
+++ LearnJava/src/datastructure/If_Else_StatementQuestions.java
@@ -1,45 +1,22 @@
 package datastructure;
-import java.util.Locale;
+
 import java.util.Scanner;
 
 public class If_Else_StatementQuestions {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter your income in lac");
-        float income=sc.nextFloat();
-        if (income>=1.0f && income<2.5f){
-            float tax=0.0f;
-            float taxpay=(income)*tax;
-            float yourincome=(income)-taxpay;
-            System.out.println("you paid tax of "+taxpay);
-            System.out.println("your income "+yourincome);
+        Scanner sc = new Scanner(System.in);
+       String website=sc.nextLine();
+       if (website.endsWith(".org")){
+           System.out.println("organizational website");
+       }
+       else if (website.endsWith(".in")){
+            System.out.println("indian website");
         }
-        else if (income>=2.5f && income<5.0f){
-            float tax=0.05f;
-            float taxpay=(income)*tax;
-            float yourincome=(income)-taxpay;
-            System.out.println("you paid tax of "+taxpay);
-            System.out.println("your income "+yourincome);
-
+       else if (website.endsWith(".com")){
+            System.out.println("commercial website");
         }
-
-        else if (income>=5.0f && income<10.0f){
-            float tax=0.2f;
-            float taxpay=(income)*tax;
-            float yourincome=(income)-taxpay;
-            System.out.println("you paid tax of "+taxpay);
-            System.out.println("your income "+yourincome);
-        }
-        else{
-            float tax=0.3f;
-            float taxpay=(income)*tax;
-            float yourincome=(income)-taxpay;
-            System.out.println("you paid tax of "+taxpay);
-            System.out.println("your income "+yourincome);
-        }
-
-
-
-
+       else {
+           System.out.println("junk website");
+       }
     }
 }
