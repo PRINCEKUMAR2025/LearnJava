7f03a576d308dbcdff7dd1dafd7341d895785d17
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-13 16:28:56 +0530

    income tax to be paid on the basis of your income

--- LearnJava/src/datastructure/If_Else_StatementQuestions.java
+++ LearnJava/src/datastructure/If_Else_StatementQuestions.java
@@ -5,19 +5,41 @@ import java.util.Scanner;
 public class If_Else_StatementQuestions {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter your income in lac");
+        float income=sc.nextFloat();
+        if (income>=1.0f && income<2.5f){
+            float tax=0.0f;
+            float taxpay=(income)*tax;
+            float yourincome=(income)-taxpay;
+            System.out.println("you paid tax of "+taxpay);
+            System.out.println("your income "+yourincome);
+        }
+        else if (income>=2.5f && income<5.0f){
+            float tax=0.05f;
+            float taxpay=(income)*tax;
+            float yourincome=(income)-taxpay;
+            System.out.println("you paid tax of "+taxpay);
+            System.out.println("your income "+yourincome);
 
-        int mathspercentile=sc.nextInt();
-        int physicspercentile=sc.nextInt();
-        int chemestrypercentile=sc.nextInt();
-        int AVERAGEpercentile=(mathspercentile+physicspercentile+chemestrypercentile)/3;
-
-        if (mathspercentile>=40 && physicspercentile>=40 && chemestrypercentile>=40 && AVERAGEpercentile>=50){
-            System.out.println("congo you pass !");
         }
 
+        else if (income>=5.0f && income<10.0f){
+            float tax=0.2f;
+            float taxpay=(income)*tax;
+            float yourincome=(income)-taxpay;
+            System.out.println("you paid tax of "+taxpay);
+            System.out.println("your income "+yourincome);
+        }
         else{
-            System.out.println("you don`t pass ");
+            float tax=0.3f;
+            float taxpay=(income)*tax;
+            float yourincome=(income)-taxpay;
+            System.out.println("you paid tax of "+taxpay);
+            System.out.println("your income "+yourincome);
         }
+
+
+
 
     }
 }
