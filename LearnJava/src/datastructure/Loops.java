8af63f190efe0b52b90e0206988f880c7e4352fe
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-23 14:03:47 +0530

    reverse nastural numbers till nn

--- LearnJava/src/datastructure/Loops.java
+++ LearnJava/src/datastructure/Loops.java
@@ -6,9 +6,9 @@ public class Loops {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
-        for (int i=0;i<n;i++){
-            int odd=2*i+1;
-            System.out.println(odd);
+        for (int i=n;i>=0;i--){
+//            int odd=2*i+1;
+            System.out.println(i);
         }
     }
 }
