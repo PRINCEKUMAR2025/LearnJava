a865d64817b10a622360738b78a6f3fcf52316b4
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-23 13:23:46 +0530

    simple while loops

--- LearnJava/src/datastructure/Loops.java
+++ LearnJava/src/datastructure/Loops.java
@@ -3,9 +3,10 @@ package datastructure;
 public class Loops {
     public static void main(String[] args) {
         int i=0;
-        while(i<=10){
+        do {
             System.out.println(i);
             i++;
         }
+        while(i<=10);
     }
 }
