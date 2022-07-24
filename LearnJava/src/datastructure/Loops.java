2698cfa2448cba91c1f3cd5ad1651387997cc589
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-24 17:23:09 +0530

    factorial of any number

--- LearnJava/src/datastructure/Loops.java
+++ LearnJava/src/datastructure/Loops.java
@@ -1,16 +1,24 @@
 package datastructure;
 
 import java.util.Scanner;
+/*
+5! = 5*(5-1)*(5-2)*(5-3)*(5-4)
+1*5
+5*4=20
+20*3=60
+60*2=120
+120*1=120
 
+ */
 public class Loops {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int n=10;
-        int sum=0;
-        for (int i=1;i<n;i++){
-            int even=2*i;
-            System.out.println(even);
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        int factorial=1;
+        for (int i=0;i<n;i++){
+            factorial=  factorial * (n-i);
         }
-//        System.out.println(sum);
+        System.out.println(factorial);
+
     }
 }
