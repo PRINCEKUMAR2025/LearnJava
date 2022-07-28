047e04763804160cba1c41448cbe056ffac3b4d5
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-07-28 12:05:06 +0530

    2d array

--- LearnJava/src/datastructure/Array.java
+++ LearnJava/src/datastructure/Array.java
@@ -7,12 +7,22 @@ public class Array {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int []marks=new int[10];
-        for (int i=0;i<10;i++){
-            marks [i]=sc.nextInt();
-        }
+        int [][]flat=new int[10][10];
+        flat[0][0]=100;
+        flat[0][1]=101;
+        flat[0][2]=102;
+        flat[1][0]=200;
+        flat[1][1]=201;
+        flat[1][2]=202;
 
-        for (int i=0;i<10;i++){
-            System.out.println(marks[i]);
+
+        System.out.println(flat[0][2]);
+//        for (int i=0;i<10;i++){
+//            marks [i]=sc.nextInt();
+//        }
+//
+//        for (int i=0;i<10;i++){
+//            System.out.println(marks[i]);
         }
     }
-}
+
