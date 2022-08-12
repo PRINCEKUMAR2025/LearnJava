aa03019d120713d210f46a354e64472970be6b3d
PRINCEKUMAR2025 <prince.kumar2021a@vitstudent.ac.in>
2022-08-12 11:10:46 +0530

    creating new java class

--- LearnJava/src/datastructure/ObjectOrientedP.java
+++ LearnJava/src/datastructure/ObjectOrientedP.java
@@ -2,31 +2,43 @@ package datastructure;
 
 import java.util.Scanner;
 
-class Student{
-    int regno;
-    String name;
-    int year;
-    public void printdetails(){
-        System.out.println("this is my regno" +regno);
-        System.out.println("this is my name" +name);
-        System.out.println("this is my year" +year);
-    }
-}
-
 public class ObjectOrientedP {
     public static void main(String[] args) {
         System.out.println("creating a new java class created upper side and now creating a new object in public class ");
         Scanner sc =new Scanner(System.in);
+
+
         Student Prince=new Student();
         Prince.regno=5326;
         Prince.name="Prince";
         Prince.year=2;
-        System.out.println(Prince.name);
-        System.out.println(Prince.regno);
-        System.out.println(Prince.year);
+        Prince.cgpa=8;
+
+        Student deep=new Student();
+        deep.regno=5364;
+        deep.name="Deep singh rathor";
+        deep.year=2;
+        deep.cgpa=9.26;
+//        System.out.println(Prince.name);
+//        System.out.println(Prince.regno);
+//        System.out.println(Prince.year);
         Prince.printdetails();
 
         }
 
+}
+class Student{
+    int regno;
+    String name;
+    int year;
+    double cgpa;
+    public void printdetails(){
+        System.out.println("this is my regno " +regno);
+        System.out.println("this is my name " +name);
+        System.out.println("this is my yea r" +year);
+    }
+    public void printcgpa(){
+        System.out.println(cgpa);
+    }
 }
 
