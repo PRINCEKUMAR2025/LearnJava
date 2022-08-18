0e4aff74f93c43a6197357c947687e49faf205df
Prince <prince@MSI.localdomain>
2022-08-19 00:28:16 +0530

    Entering name and storing privately

--- LearnJava/src/datastructure/GettersSetters.java
+++ LearnJava/src/datastructure/GettersSetters.java
@@ -6,25 +6,28 @@ public class GettersSetters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         employe prince = new employe();
-        prince.setName("prince");
-        prince.setId(5326);
-        System.out.println(prince.getId());
+//        prince.setName("prince");
+//        prince.setId(5326);
+        prince.setName();
+        prince.setId();
         System.out.println(prince.getName());
+        System.out.println(prince.getId());
 
     }
 }
 
 
 class employe{
+    Scanner sc=new Scanner(System.in);
     private int id;
     private String name;
 
-    public void setId(int i){
-       id=i;
+    public void setId(){
+       int id =sc.nextInt();
     }
 
-    public void setName(String n){
-        name=n;
+    public void setName(){
+        name=sc.nextLine();
     }
     public String getName(){
         return name;
