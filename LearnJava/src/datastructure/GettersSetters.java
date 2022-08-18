05b5c697c6790c72efb8acf599d60fb199f32e18
Prince <prince@MSI.localdomain>
2022-08-18 23:54:19 +0530

    getterssettersprivateclass

--- LearnJava/src/datastructure/GettersSetters.java
+++ LearnJava/src/datastructure/GettersSetters.java
@@ -6,6 +6,11 @@ public class GettersSetters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         employe prince = new employe();
+        prince.setName("prince");
+        prince.setId(5326);
+        System.out.println(prince.getId());
+        System.out.println(prince.getName());
+
     }
 }
 
@@ -14,7 +19,18 @@ class employe{
     private int id;
     private String name;
 
-    public void setId(){
-        
+    public void setId(int i){
+       id=i;
+    }
+
+    public void setName(String n){
+        name=n;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
     }
 }
