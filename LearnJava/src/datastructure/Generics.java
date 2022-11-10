51bfea6731832728bd7fe1fccd3916259df8f64e
PRINCEKUMAR2025 <princeiitbombay@gmail.com>
2022-11-11 00:37:47 +0530

    generics

---
+++ LearnJava/src/datastructure/Generics.java
@@ -0,0 +1,23 @@
+package datastructure;
+import java.util.*;
+//import java.util.ArrayList;
+public class Generics {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("Hello");
+        arrayList.add("prince");
+        arrayList.add("this side");
+
+        String a=(String)arrayList.get(0);
+        String b=(String)arrayList.get(1);
+        String c=(String)arrayList.get(2);
+//    ArrayList<Integer> arrayList=new ArrayList();
+//    arrayList.add(10);
+//    arrayList.add("prince");
+//    arrayList.add(20);
+//        System.out.println(arrayList);
+//        int a=(int )arrayList.get(0);
+//        System.out.println(a);
+        System.out.println(a +" "+b+" "+c);
+    }
+}
